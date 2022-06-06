756697c4d5baa6d2a7f185726b1a51741b2dd1a2
ttthien27 <ttthien.20@gmail.com>
2022-06-06 18:28:05 +0700

    Edit example Navigation Component

--- app/src/main/java/com/example/sampleproject/view/activity/NavigationExampleActivity.java
+++ app/src/main/java/com/example/sampleproject/view/activity/NavigationExampleActivity.java
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -19,51 +22,36 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class NavigationExampleActivity extends AppCompatActivity {
 
     private ActionBar toolbar;
+    private NavController navController;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_example);
 
-        toolbar = getSupportActionBar();
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.nav_view);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        toolbar.setTitle("first");
-
-
-    }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment;
-            switch (item.getItemId()) {
-                case R.id.navigation_first:
-                    toolbar.setTitle("first");
-                    fragment = new FragmentFirstFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_second:
-                    toolbar.setTitle("second");
-                    fragment = new FragmentSecondFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_third:
-                    toolbar.setTitle("third");
-                    return true;
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.navigation_first:
+                        navController.navigate(R.id.fragmentFirstFragment);
+                        return true;
+                    case R.id.navigation_second:
+                        navController.navigate(R.id.fragmentSecondFragment);
+                        return true;
+                    case R.id.navigation_third:
+                        navController.navigate(R.id.fragmentHomeFragment3);
+                        return true;
+                }
+                return false;
             }
-            return false;
-        }
-    };
-
-    private void loadFragment(Fragment fragment) {
-        // load fragment
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.nav_host_fragment, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        });
     }
+
 }
